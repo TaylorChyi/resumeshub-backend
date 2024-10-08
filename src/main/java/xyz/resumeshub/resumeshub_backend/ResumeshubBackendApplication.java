6ef9912eae61db2964a22dc3bc4828d4c31ab487
Qi Tianyue <taylor_chyi@icloud.com>
2024-10-08 23:46:30 +0800

    - feature: initialize resumeshub-backend;

---
+++ src/main/java/xyz/resumeshub/resumeshub_backend/ResumeshubBackendApplication.java
@@ -0,0 +1,13 @@
+package xyz.resumeshub.resumeshub_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeshubBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeshubBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/resumeshub/resumeshub_backend/ResumeshubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.resumeshub.resumeshub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeshubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
